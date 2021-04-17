c2bce92b88f6104b679cfe8da935f7355d1d9619
Amir Cherkaoui <amir.cherkaoui-eddeqaqi@polymtl.ca>
2021-04-17 17:20:22 -0400

    Clarification noms

--- SingletonPattern/SingletonPattern/TestsUnitaires.cpp
+++ SingletonPattern/SingletonPattern/TestsUnitaires.cpp
@@ -5,19 +5,19 @@
 
 TEST_CASE("L'analyse de performance des eleves fonctionne correctement")
 {
-	unique_ptr<Etudiant> premier = make_unique<Etudiant>("Stephane", 20);
-	unique_ptr<Etudiant> deuxieme = make_unique<Etudiant>("Amir", 21);
+	unique_ptr<Etudiant> premierEtudiant = make_unique<Etudiant>("Stephane", 20);
+	unique_ptr<Etudiant> deuxiemeEtudiant = make_unique<Etudiant>("Amir", 21);
 
-	REQUIRE(premier->AnalyserPerformance() == true);
-	REQUIRE(deuxieme->AnalyserPerformance() == true);
+	REQUIRE(premierEtudiant->AnalyserPerformance() == true);
+	REQUIRE(deuxiemeEtudiant->AnalyserPerformance() == true);
 }
 
 TEST_CASE("Deux eleves font appel au meme objet Analyse pour l'analyse de performance")
 {
-	unique_ptr<Etudiant> premier = make_unique<Etudiant>("Stephane", 20);
-	unique_ptr<Etudiant> deuxieme = make_unique<Etudiant>("Amir", 21);
+	unique_ptr<Etudiant> premierEtudiant = make_unique<Etudiant>("Stephane", 20);
+	unique_ptr<Etudiant> deuxiemeEtudiant = make_unique<Etudiant>("Amir", 21);
 
-	REQUIRE(premier->verifierSigleton() == deuxieme->verifierSigleton());
+	REQUIRE(premierEtudiant->verifierSigleton() == deuxiemeEtudiant->verifierSigleton());
 }
 
 
