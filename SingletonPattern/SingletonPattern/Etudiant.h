6134815515a08dd947fcc1613ce9dea1eaa64713
Amir Cherkaoui <60159779+cherkamir@users.noreply.github.com>
2021-04-18 01:06:04 -0400

    Ajout de commentaires dans Etudiant.h

--- SingletonPattern/SingletonPattern/Etudiant.h
+++ SingletonPattern/SingletonPattern/Etudiant.h
@@ -3,16 +3,25 @@
 class Etudiant
 {
 public:
+	//Constructeur d'Etudiant
 	Etudiant (string nom, int age) :
 		nom_(nom)
 		, age_(age)
 	{}
-
+	
+	/*
+	Methode qui appelle la methode ProgrammeAnalyse() de Analyse
+	pour analyser la performance jouee par l'etudiant
+	*/
 	bool AnalyserPerformance()
 	{
 		return Analyse::getInstance()->programmeAnalyse();
 	}
-
+	/*
+	Cette methode va nous servir dans les tests untiaires, verifier
+	que chaque instance d'etudiant utilise la meme classe Analyse pour
+	analyser la performance.
+	*/
 	const Analyse* verifierSigleton()
 	{
 		return Analyse::getInstance()->adresseSingleton();
